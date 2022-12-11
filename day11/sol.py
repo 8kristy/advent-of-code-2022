dab83f56134a4bde2afe1916ef9993185724c2ef
kristina <8kristy@gmail.com>
2022-12-11 11:24:41 +0000

    Day 11

---
+++ day11/sol.py
@@ -0,0 +1,54 @@
+monkeys = []
+
+class Monkey():
+
+    def __init__(self, items, operation, test, throw_true, throw_false):
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.throw_true = throw_true
+        self.throw_false = throw_false
+        self.items_inspected = 0
+
+    def throw_items(self):
+        for item in self.items:
+            self.items_inspected += 1
+            num = 0
+            if self.operation[1] == "":
+                num = item
+            else:
+                num = int(self.operation[1])
+            
+            if self.operation[0] == "+":
+                item = item + num
+            else:
+                item = item * num
+
+            item = int(item / 3)
+
+            if item % self.test == 0:
+                monkeys[self.throw_true].items.append(item)
+            else:
+                monkeys[self.throw_false].items.append(item)
+
+        self.items = []
+
+with open("input", 'r') as f:
+    for _ in range(8):
+        f.readline()
+        items = [int(x) for x in f.readline().strip().split(":")[1].split(",")]
+        (operator, num) = f.readline().strip().split("old")[1].split(" ")[1:]
+        test = int(f.readline().strip().split(" ")[-1])
+        throw_true = int(f.readline().strip().split(" ")[-1])
+        throw_false = int(f.readline().strip().split(" ")[-1])
+        monkeys.append(Monkey(items, (operator, num), test, throw_true, throw_false))
+        f.readline()
+
+for i in range(20):
+    for monkey in monkeys:
+        monkey.throw_items()
+
+inspected = [monkey.items_inspected for monkey in monkeys]
+inspected.sort(reverse=True)
+print(inspected[0] * inspected[1])
+        

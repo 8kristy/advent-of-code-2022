7f7ff2efc95a8345c4fbf7aecec161dfed17b6d8
kristina <8kristy@gmail.com>
2022-12-14 08:38:14 +0000

    Day 14

---
+++ day14/sol.py
@@ -0,0 +1,50 @@
+rocks = []
+
+with open("input", 'r') as f:
+    for line in f.readlines():
+        line = line.strip().split("->")
+        line = [x.strip().split(",") for x in line]
+
+        for i in range(len(line) - 1):
+            x1, y1 = [int(x) for x in line[i]]
+            x2, y2 = [int(x) for x in line[i + 1]]
+
+            if x1 == x2:
+                for y in range(min(y1, y2), max(y1, y2) + 1):
+                    rocks.append((x1, y))
+
+            elif y1 == y2:
+                for x in range(min(x1, x2), max(x1, x2) + 1):
+                    rocks.append((x, y1))
+
+rocks = list(set(rocks))
+max_depth = max([x[1] for x in rocks])
+
+sand = []
+
+while True:
+    sand_x, sand_y = 500, 0
+    while True:
+        if sand_y > max_depth:
+            break
+
+        if (sand_x, sand_y + 1) not in rocks and (sand_x, sand_y + 1) not in sand:
+            sand_y += 1
+        elif (sand_x - 1, sand_y + 1) not in rocks and (sand_x - 1, sand_y + 1) not in sand:
+            sand_x -= 1
+            sand_y += 1
+        elif (sand_x + 1, sand_y + 1) not in rocks and (sand_x + 1, sand_y + 1) not in sand:
+            sand_x += 1
+            sand_y += 1
+        else:
+            sand.append((sand_x, sand_y))
+            break
+
+    if sand_y > max_depth:
+        break
+    
+
+print(len(sand))
+
+
+          
